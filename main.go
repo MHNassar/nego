a4edfa2ef281cf93a8cc092292a2ebc0ed84a898
Mahmoud Nassar <neso@Mahmouds-MacBook-Pro.local>
2020-02-17 23:32:55 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, Gophers Edit !"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	httpServer := http.Server{
+		Addr:    ":3000",
+		Handler: mux,
+	}
+
+	httpServer.ListenAndServe()
+}
